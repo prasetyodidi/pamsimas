450f66231292d106ac7da7a1b88f969287136d2e
Didi Prasetyo <didiprasetyo360@gmail.com>
2024-05-24 22:11:00 +0700

    feat: dashboard

--- app/Http/Controllers/dashboardController.php
+++ app/Http/Controllers/dashboardController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pelanggan;
+use App\Models\Pembayaran;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class dashboardController extends Controller
 {
     public function index(){
-        return view('dashboard.dashboard');
+        $totalPelanggan = Pelanggan::count();
+        $totalPembayaran = Pembayaran::query()->where('status', 'capture')->get();
+        $user = Auth::user();
+        return view('dashboard.dashboard', compact('totalPelanggan', 'totalPembayaran', 'user'));
     }
 }
